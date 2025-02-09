23df72f89956962e7ac106e6f915174e01dcc8d2
hau pham <hau20112003z@gmail.com>
2025-02-09 19:29:55 +0700

    update

--- routers/nation.routes.js
+++ routers/nation.routes.js
@@ -1,10 +1,7 @@
 import { Router } from "express"
-import {getAllNations, getNationById, deleteNationById, putNationById, postNation} from '../controllers/nation.controller.js'
+import {getAllNations, getNationById} from '../controllers/nation.controller.js'
 
 export const route = Router()
 
 route.get('/',getAllNations)
 route.get('/:id',getNationById)
-route.delete('/:id',deleteNationById)
-route.put('/:id',putNationById)
-route.post('/',postNation)
